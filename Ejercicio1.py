7f75610ab19fae5b6b9f03e0e8086a8e4aabf102
RMSE233 <sequeirarm@uamv.edu.ni>
2025-04-08 10:53:02 -0600

    Add files via upload

---
+++ Ejercicio1.py
@@ -0,0 +1,9 @@
+#Escribe un algoritmo que declare tres variables: una para el nombre de una persona, otra para su edad y otra para su estatura. asigna valores adecuados a cada una. 
+nombre = "roger"
+edad = 19 
+estatura = 1.75 #metros 
+
+#mostramos los valores
+print("nombre:", nombre)
+print("edad:", edad)
+print("estatura:", estatura, "m")
