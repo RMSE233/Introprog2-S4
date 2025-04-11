f4abffb318e7bd0bea629892b265d45349061769
RMSE233 <sequeirarm@uamv.edu.ni>
2025-04-10 18:07:07 -0600

    Add files via upload

---
+++ ejercicio1.py
@@ -0,0 +1,9 @@
+#declarar variables
+nombre_completo = "Roger Marcel Sequeira Espinoza"
+edad = 19
+altura = 1.75
+soltero = True 
+
+print(f"Nombre completo: {nombre_completo}")
+print(f"tipo de dato: {type(nombre_completo)}")
+printf(f"{edad}") {type(edad)}
---
+++ ejercicio2.py
@@ -0,0 +1,9 @@
+#Tipos de datos
+
+cantidad = 10
+precio = 130
+total = cantidad * precio 
+
+print(f"{cantidad: 6}")
+print(f"Precio: {precio: 6}") 
+print(f"total: {total: 6}")
