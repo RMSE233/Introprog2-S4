a22fd6825240109f9726218a0d927f95917fdc96
RMSE233 <sequeirarm@uamv.edu.ni>
2025-04-10 18:26:52 -0600

    Add files via upload

---
+++ ejercicio4.py
@@ -0,0 +1,13 @@
+# Tenemos un número como texto
+texto = "25"
+
+# Lo convertimos a número entero
+numero = int(texto)
+
+# Le sumamos 1
+numero = numero + 1
+
+# Mostramos el nuevo número
+print("Ejercicio 4")
+print("El número incrementado es:", numero)
+print()
